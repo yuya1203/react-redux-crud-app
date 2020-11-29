d5b261c1fa2d6a7dfcd0329c85f75b38d5fe0702
yuya1203 <yuyaaa.1203@gmail.com>
2020-11-29 20:11:20 +0900

    implement store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux'; // storeに必要
+import { Provider } from 'react-redux'// 作成したstoreを全コンポーネントに渡すもの
 import './index.css';
-import App from './App';
+import reducer from './reducers';
+import App from '.components/App';
 import reportWebVitals from './reportWebVitals';
+
+const store = createStore(reducer)
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
