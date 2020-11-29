f29de1477fb3c8d39384debdca3329829b77463d
yuya1203 <yuyaaa.1203@gmail.com>
2020-11-29 19:47:52 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+// actionとはjavascriptのオブジェクトのこと
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export const increment = () => ({
+		type: INCREMENT
+})
+export const decrement = () => ({
+		type: DECREMENT
+})
