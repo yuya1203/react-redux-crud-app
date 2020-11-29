b9016d6f9d7799f9d8f78a0f564049258509fb1d
yuya1203 <yuyaaa.1203@gmail.com>
2020-11-29 15:23:45 +0900

    learn jsx

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React, {Component} from 'react';
 
+// import Reactはjsxをトランスパイル後にReact.dom.Renderを使用して仮装domを描画するのでjsxを使う場合には必須
+// returnするjsxは一つのタグでなければならない
 class App extends Component {
   render() {
-    return <div><h1>Hello, world!!!!!</h1></div>;
+    return (
+      <div>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => { console.log("I am clicked!") }} />
+      </div>
+    )
+
+
+    // const greeting = "Hi, Tom";
+    // const dom = <h1>{greeting}</h1>;
+    // return dom;
+    // return <h1>Hello, world!!!!!</h1>;
   }
 }
 
