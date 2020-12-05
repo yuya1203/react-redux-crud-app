74bc85bd53204d4d7a8ac8f4ff867c44f8b6560f
yuya1203 <yuyaaa.1203@gmail.com>
2020-12-06 00:57:39 +0900

    apply material-ui to events_new and events_show components

--- src/components/events_show.js
+++ src/components/events_show.js
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { Field, reduxForm } from 'redux-form'
 import { Link } from 'react-router-dom' // 画面遷移などのリンクで使用する
 import { getEvent, deleteEvent, putEvent } from '../actions';
+import RaisedButton from 'material-ui/RaisedButton'
+import TextField from 'material-ui/TextField'
 
 class EventsShow extends Component {
   constructor(props) {
@@ -20,10 +22,19 @@ class EventsShow extends Component {
     const { input, label, type, meta: { touched, error } } = field
 
     return (
-      <div>
-        <input {...input} placeholder={label} type={type} />
-        {touched && error && <span>{error}</span>}
-    </div>)
+      <TextField
+        hintText={label}
+      floatingLabelText={label}
+      type={type}
+      errorText={touched && error}
+      {...input}
+      fullWidth={true}
+      />
+      // <div>
+      //   <input {...input} placeholder={label} type={type} />
+      //   {touched && error && <span>{error}</span>}
+      // </div>
+    )
   }
 
   async onDeleteClick() {
@@ -39,16 +50,21 @@ class EventsShow extends Component {
   render() {
 
     const { handleSubmit, pristine, submitting, invalid } = this.props
+    const style = { margin: 12 }
 
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
         <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
-        <div>
+
+        <RaisedButton label="Submit" type="submit" style={style} disabled={pristine || submitting || invalid} />
+        <RaisedButton label="Cancel" style={style} containerElement={<Link to="/" />}/>
+        <RaisedButton label="Delete" style={style} onClick={this.onDeleteClick} />
+        {/* <div>
           <input type="submit" value="Submit" disabled={pristine || submitting || invalid} />
           <Link to="/">Cancel</Link>
           <Link to="/" onClick={this.onDeleteClick} >Delete</Link>
-        </div>
+        </div> */}
       </form>
     )
   }
