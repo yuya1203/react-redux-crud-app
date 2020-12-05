11feab3ff46c9d6222223613dc7fabf07a3fd1f2
yuya1203 <yuyaaa.1203@gmail.com>
2020-12-05 19:32:35 +0900

    update

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -25,14 +25,14 @@ class EventsNew extends Component {
   }
   render() {
 
-    const { handleSubmit } = this.props
+    const { handleSubmit, pristine, submitting } = this.props
 
     return (
       <form onSubmit={handleSubmit(this.onSubmit)}>
         <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
         <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
         <div>
-          <input type="submit" value="Submit" disabled={false} />
+          <input type="submit" value="Submit" disabled={pristine || submitting} />
           <Link to="/">Cancel</Link>
         </div>
       </form>
